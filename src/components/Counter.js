f5a8316283ed8ca720ab9258c0ec001bba080017
Takeo Tsumura <tsumutake@gmail.com>
2021-07-18 14:20:28 +0900

    Handle plural

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,7 @@ function Counter() {
 
     return (
         <div>
-            <p>You clicked {count} times.</p>
+            <p>You clicked {count} {(count <= 1) ? "time" : "times"}.</p>
             <button onClick={() => setCount(count + 1)}>
                 Click Me
             </button>
